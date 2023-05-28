773907942ded49102f820036c7b6f514d8806046
Bassil Younes <83778381+Byounes123321@users.noreply.github.com>
2023-05-28 18:22:19 -0400

    commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,49 +8,44 @@
     <title>Food Reviews</title>
     <link rel="stylesheet" href="./style.css">
 </head>
+
 <body>
     <h1>Food Review</h1>
 
-<?php 
+    <?php
 
-$connect = mysqli_connect(
-    "sql211.epizy.com",
-    "epiz_33373324",
-    "v56TCDTvxvOod1",
-    "epiz_33373324_reviews"
-);
 
-mysqli_set_charset($connect, 'UTF8');
 
-$revQuery = 'SELECT Reviews.*, Restaurants.rest_name
+    mysqli_set_charset($connect, 'UTF8');
+
+    $revQuery = 'SELECT Reviews.*, Restaurants.rest_name
              FROM Reviews
              JOIN Restaurants ON Reviews.rest_Id = Restaurants.rest_Id
              ORDER BY Reviews.date';
 
-$revResult = mysqli_query($connect, $revQuery);
-if (!$revResult) {
-    die("Database error: " . mysqli_error($connect));
-}
+    $revResult = mysqli_query($connect, $revQuery);
+    if (!$revResult) {
+        die("Database error: " . mysqli_error($connect));
+    }
 
-echo '<div class="container">';
-while ($record = mysqli_fetch_assoc($revResult)) {
+    echo '<div class="container">';
+    while ($record = mysqli_fetch_assoc($revResult)) {
 
-    echo '<div class="receiptContainer">
-    <h2 class="logo">'.$record['rest_name'].'</h2>
-    <p class="orderNum">ORDER #'.rand(1,999).'</p>
-    <p class="date">Date: '.$record['date'].'</p>
+        echo '<div class="receiptContainer">
+    <h2 class="logo">' . $record['rest_name'] . '</h2>
+    <p class="orderNum">ORDER #' . rand(1, 999) . '</p>
+    <p class="date">Date: ' . $record['date'] . '</p>
     <hr class= "dotted" />
-    <p>What we got: '.$record['dish'].'</p>
-    <p>Notes: '.$record['notes'].'</p>
-    <p class="rating">Rating: '.$record['rating'].'/10&#9733</p>
+    <p>What we got: ' . $record['dish'] . '</p>
+    <p>Notes: ' . $record['notes'] . '</p>
+    <p class="rating">Rating: ' . $record['rating'] . '/10&#9733</p>
    
 </div>';
+    }
 
+    echo '</div>';
 
-}
-
-echo '</div>';
-
-?>
+    ?>
 </body>
+
 </html>
